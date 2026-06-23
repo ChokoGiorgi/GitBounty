f5aab697ee5e92208f82e44687d543cca6492a2b
TheGreatGearBox29 <mkuru23@freeuni.edu.ge>
2026-06-23 14:52:35 +0400

    Feat/get issue (#118)
    * Remove DS Store files
    
    * Added get issue endpoint
    
    * Add get issue controller endpoint (i forgot in the previous commit ddd)
    
    * Remove Spring exceptions from issue service
    
    * Move issue authentication check to controller
    
    ---------
    
    Co-authored-by: Giorgi Chokoshvili <73429499+ChokoGiorgi@users.noreply.github.com>

--- Backend/src/main/java/org/gitbounty/gitbountybackend/exception/IssueNotFoundException.java
+++ Backend/src/main/java/org/gitbounty/gitbountybackend/exception/IssueNotFoundException.java
@@ -1,6 +1,10 @@
 package org.gitbounty.gitbountybackend.exception;
 
 public class IssueNotFoundException extends RuntimeException {
+    public IssueNotFoundException(String message) {
+        super(message);
+    }
+
     public IssueNotFoundException(Long id) {
         super("Issue not found with id: " + id);
     }
