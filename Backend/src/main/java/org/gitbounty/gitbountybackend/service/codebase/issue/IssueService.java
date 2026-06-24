2c09c13339e62ff59b72b68b04c5eafd95d96dbc
TheGreatGearBox29 <mkuru23@freeuni.edu.ge>
2026-06-24 22:47:34 +0400

    Add issue exception handling and tests for issue and several other exceptions (#125)
    Co-authored-by: Acro <nikalomsadze056@gmail.com>

--- Backend/src/main/java/org/gitbounty/gitbountybackend/service/codebase/issue/IssueService.java
+++ Backend/src/main/java/org/gitbounty/gitbountybackend/service/codebase/issue/IssueService.java
@@ -12,7 +12,6 @@ import org.gitbounty.gitbountybackend.service.codebase.CodebaseService;
 import org.gitbounty.gitbountybackend.service.user.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import org.gitbounty.gitbountybackend.exception.IssueNotFoundException;
 
 import java.util.List;
